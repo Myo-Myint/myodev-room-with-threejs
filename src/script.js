9856d21d5167e636749bf40ce3d4658b350e7f89
Myo Myint Thu <myomyintthu@Myos-MacBook-Air.local>
2023-10-28 22:32:46 +0100

    Attempt1: adding analytica

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import './style.css'
 import Experience from './Experience/Experience.js'
-
+import { Analytics } from '@vercel/analytics/react';
 window.experience = new Experience({
     targetElement: document.querySelector('.experience')
 })
